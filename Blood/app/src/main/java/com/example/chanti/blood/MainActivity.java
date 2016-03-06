de7bc8af98c486a66e192b331a453544cd9805c0
Santhosh mohan <santhosh.murarishetti@gmail.com>
2016-03-05 18:15:10 -0600

    database creation and minor gui changes in registration page

--- Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
+++ Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
@@ -1,35 +1,16 @@
 package com.example.chanti.blood;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton locButton;
-    Button btnreg;
-    Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,93 +48,11 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (item.getItemId() == R.id.action_logout) {
+            Intent s = new Intent(MainActivity.this, SplashActivity.class);
+            startActivity(s);
+        }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    public void setLocation(View v)
-    {
-        locButton = (ImageButton) findViewById(R.id.getAddressButton);
-        //Intent m = new Intent(Registration.this, map.class);
-        //startActivity(m);
-
-        geocoder = new Geocoder(this);
-        StringBuilder userAddress = new StringBuilder();
-        // Add a marker in Sydney and move the camera
-        LocationManager userCurrentLocation = (LocationManager) this
-                .getSystemService(Context.LOCATION_SERVICE);
-        LocationListener userCurrentLocationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-        LatLng userCurrentLocationCorodinates = null;
-        double latitute = 0, longitude = 0;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat
-                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //show message or ask permissions from the user.
-            return;
-        }
-        //Getting the current location of the user.
-        userCurrentLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                0, 0, userCurrentLocationListener);
-        latitute = userCurrentLocation
-                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                .getLatitude();
-        longitude = userCurrentLocation
-                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                .getLongitude();
-        userCurrentLocationCorodinates = new LatLng(latitute,longitude);
-        //Getting the address of the user based on latitude and longitude.
-        try {
-            List<Address> addresses = geocoder.getFromLocation(latitute, longitude, 1);
-            Address address = addresses.get(0);
-            //add = address.toString();
-            userAddress =  new StringBuilder();
-            for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                userAddress.append(address.getAddressLine(i)).append("\t");
-            }
-            userAddress.append(address.getCountryName()).append("\t");
-
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-
-        EditText locat = (EditText) findViewById(R.id.address);
-        locat.setText(userAddress);
-
-    }
-
-    public void register(View v)
-    {
-        btnreg = (Button) findViewById(R.id.btnRegister);
-
-        if(v.getId() == R.id.btnRegister) {
-            Intent h = new Intent(MainActivity.this, home.class);
-            startActivity(h);
-        }
-
-        else {
-
-        }
     }
 }
