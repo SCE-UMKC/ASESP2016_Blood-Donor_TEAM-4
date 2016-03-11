9b9e04ce99e31f41dea3456b9a264a396300b568
Santhosh mohan <santhosh.murarishetti@gmail.com>
2016-03-11 17:46:44 -0600

    retrival of donor list

--- Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
+++ Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
@@ -4,10 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    String cityTxt;
+    String bloodGroupTxt;
+    ImageButton searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Firebase.setAndroidContext(this);
+
     }
 
     @Override
@@ -42,5 +58,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void searchDonors(View v){
+        cityTxt = ((EditText) findViewById(R.id.city)).getText().toString();
+        bloodGroupTxt = ((Spinner) findViewById(R.id.spinnerBloodGroup)).getSelectedItem().toString();
+        searchBtn = (ImageButton) findViewById(R.id.search);
+
+        if(v.getId() == R.id.search)
+        {
+            final Firebase ref = new Firebase("https://bloodmanagement.firebaseio.com/Users");
+            final Query donorList = ref.orderByChild("address").equalTo(cityTxt);
+            Query donor = donorList.orderByChild("blood_group").equalTo(bloodGroupTxt);
+            donor.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    Log.d("data", dataSnapshot.toString());
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+
+        }
     }
 }
