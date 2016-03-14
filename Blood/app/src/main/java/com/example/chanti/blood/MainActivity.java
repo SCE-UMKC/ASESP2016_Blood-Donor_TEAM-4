e67ca3300698961c43f6001941ccf10a5fd8afb9
Santhosh mohan <santhosh.murarishetti@gmail.com>
2016-03-14 01:13:05 -0500

    UI changes in home page

--- Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
+++ Blood/app/src/main/java/com/example/chanti/blood/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.chanti.blood;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 
 import com.firebase.client.DataSnapshot;
@@ -21,13 +25,15 @@ import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     String cityTxt;
     String bloodGroupTxt;
     ImageButton searchBtn;
-    ArrayList<String> donorsList = new ArrayList<String>();
+    ArrayList<String> mobileNumberList = new ArrayList<String>();
+    final ArrayList<HashMap<String, String>> donorsList = new ArrayList<HashMap<String, String>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Firebase.setAndroidContext(this);
-        final ArrayAdapter adaptor = new ArrayAdapter<String>(this, R.layout.activity_listview, donorsList);
+        SimpleAdapter adaptor = new SimpleAdapter(this, donorsList, R.layout.activity_listview, new String[]{"userName", "address", "blood_group"}, new int[]{R.id.donorName, R.id.donorAddress, R.id.donorBloodGroup});
         ListView listView = (ListView) findViewById(R.id.bloodDonorsList);
         listView.setAdapter(adaptor);
     }
@@ -67,13 +73,12 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void searchDonors(View v){
+    public void searchDonors(View v) {
         cityTxt = ((EditText) findViewById(R.id.city)).getText().toString();
         bloodGroupTxt = ((Spinner) findViewById(R.id.spinnerBloodGroup)).getSelectedItem().toString();
         searchBtn = (ImageButton) findViewById(R.id.search);
 
-        if(v.getId() == R.id.search)
-        {
+        if (v.getId() == R.id.search) {
             final Firebase ref = new Firebase("https://bloodmanagement.firebaseio.com/Users");
             final Query donorQuery = ref.orderByChild("address").equalTo(cityTxt);
             //Query donor = donorList.orderByChild("blood_group").equalTo(bloodGroupTxt);
@@ -82,17 +87,17 @@ public class MainActivity extends AppCompatActivity {
                 public void onDataChange(DataSnapshot dataSnapshot) {
 
                     for (DataSnapshot blood : dataSnapshot.getChildren()) {
-                        List<String> l = new ArrayList<String>();
                         String name = null;
                         if (blood.child("blood_group").getValue().equals(bloodGroupTxt)) {
-                            l.add(blood.child("user_name").getValue().toString());
+                            HashMap<String, String> m = new HashMap<String, String>();
+
                             name = blood.child("first_name").getValue().toString() + " " + blood.child("last_name").getValue().toString();
-                            l.add(name);
-                            l.add(blood.child("mobile").getValue().toString());
-                            l.add(blood.child("address").getValue().toString());
-                            l.add(blood.child("blood_group").getValue().toString());
+                            m.put("userName", name);
+                            m.put("address", blood.child("address").getValue().toString());
+                            m.put("blood_group", blood.child("blood_group").getValue().toString());
+                            mobileNumberList.add(blood.child("mobile").getValue().toString());
+                            donorsList.add(m);
                         }
-                        donorsList.add(l.toString());
                     }
                     Log.d("data", donorsList.toString());
 
@@ -103,8 +108,23 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
-
-
         }
+    }
+
+
+    public void onClickCall(ListView l, View v,int position, long id ) {
+        String number = mobileNumberList.get(position);
+        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 }
